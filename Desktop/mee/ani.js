c515222ce49c4f2be3225c7e65645005a6f7409e
fevourfon <fonfevour@gmail.com>
2026-02-14 16:39:49 +0100

    Update ani.js

--- Desktop/mee/ani.js
+++ Desktop/mee/ani.js
@@ -13,14 +13,32 @@ btn.style.transform = 'translateX(200px)';
 
 }, 2000);*/
 
-function movex(element, amount, delay, callback) {
+function movex(element, amount, delay, onsucces, onfailure) {
+    const bodyboundary = document.body.clientWidth;
+    const elright = element.getBoundingClientRect().right;
+    const elleft = element.getBoundingClientRect().left;
+    const elwidth = element.offsetWidth;
 
-    setTimeout(() => {
-        element.style.transform = `translateX(${amount}px)`
-    }, delay);
+    if (elright + amount > bodyboundary) {
+
+        onfailure
+    } else {
+        setTimeout(() => {
+            element.style.transform = `translateX(${amount+elleft}px)`
+            onsuccess();
+        }, delay);
+    }
 }
-movex(btn, 600, 2000, () => {
-    movex(btn, 100, 2000, () => {
-        movex(btn, 200, 2000);
-    })
+//movex(btn, 100, 1000, () => {
+//movex(btn, 100, 1000, () => {
+//movex(btn, 100, 1000, () => {h
+//movex(btn, 1000, 1000);
+
+
+
+//});
+//});
+//});
+movex(btn, 100, 1000, () => {}, () => {
+    alert('cant move');
 });
